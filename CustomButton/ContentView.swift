e6ccffea1f7cfc32e0d38e4aa01ed52e31d4a66c
ahmed hussien <133219145+ahmedhusseinDBS@users.noreply.github.com>
2025-01-26 16:55:29 +0200

    Initial Commit

---
+++ CustomButton/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomButton
+//
+//  Created by ahmed hussien on 26/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomButton/CustomButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomButtonApp.swift
+//  CustomButton
+//
+//  Created by ahmed hussien on 26/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct CustomButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
